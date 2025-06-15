5606f536f5826eef10305dd06b085669f4383bba
Whoistolu <digittolu25@gmail.com>
2025-06-15 23:40:47 +0100

    Resolve Lint errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
    def current_user
-        return nil unless request.headers['Authorization']
-        token = request.headers['Authorization'].split(' ').last
+        return nil unless request.headers["Authorization"]
+        token = request.headers["Authorization"].split(" ").last
         decoded = JWT.decode(token, Rails.application.secret_key_base)[0]
         User.find(decoded["user_id"])
     rescue
