8a261fd2995ce61e628ed6091c876423d15a69aa
Whoistolu <digittolu25@gmail.com>
2026-02-19 11:43:33 +0100

    Generate Tickets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_15_222517) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_19_102311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "status"
+    t.bigint "customer_id", null: false
+    t.bigint "assigned_agent_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["assigned_agent_id"], name: "index_tickets_on_assigned_agent_id"
+    t.index ["customer_id"], name: "index_tickets_on_customer_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_15_222517) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "tickets", "users", column: "assigned_agent_id"
+  add_foreign_key "tickets", "users", column: "customer_id"
 end
