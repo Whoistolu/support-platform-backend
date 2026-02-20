8953ea9fdf67cc3c549fd3cd2bd71117fb76c04f
Whoistolu <digittolu25@gmail.com>
2026-02-20 03:25:31 +0100

    Generate Comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_19_102311) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_20_022351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "ticket_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ticket_id"], name: "index_comments_on_ticket_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "title"
@@ -40,6 +50,8 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_19_102311) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "tickets"
+  add_foreign_key "comments", "users"
   add_foreign_key "tickets", "users", column: "assigned_agent_id"
   add_foreign_key "tickets", "users", column: "customer_id"
 end
